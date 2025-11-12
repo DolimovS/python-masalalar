890fe49120fb39d4c6bd699f890c97460aeca43c
DolimovS <samandardolimov71@gmail.com>
2025-11-12 22:10:30 +0500

    first commit

---
+++ 1.2/main.py
@@ -0,0 +1,60 @@
+# text=input("Matnni kiriting:")
+# print("Kititilgan matn:",text)  
+
+
+# son=input("Sonni kitriting:")
+# print("Siz kiritgan son:",son)  
+
+
+# a=int(input("a="))
+# b=int(input("b="))
+
+# s=a+b
+# d=a-b
+# k=a*b
+
+# print("Yig'indi:",s ,"\nAyirma:",d ,"\nKo'paytma:",k)
+
+# x=float(input("x="))
+# y=float(input("y="))
+
+# s=(abs(x)-abs(y))/(1+abs(x*y))
+# print(s)
+
+
+# a=int(input("Kubni qirrasini kiriting:"))
+
+# s=a*a
+# v=s*6
+
+# print("Yon sirti yuzasi :",s , "\nTo'la sirti yuzasi:",v)
+
+# import math
+
+# a=int(input("a="))
+# b=int(input("b="))
+
+# s=(a+b)/2
+# y=math.sqrt(a*b)
+
+# print("O'rta arifmetigi:",s,"\nO'rta geometrigi",y)
+
+# import math
+# a=int(input("a="))
+# b=int(input("b="))
+
+# c=math.sqrt(pow(a,2)+pow(b,2))
+
+# s=(a+b)/2
+# print("Gipatenuza:", c ,"\nYuza",s)
+
+# 9
+
+# t1=int(input("t1:"))
+# t2=int(input("t2:"))
+# v1=int(input("v1:"))
+# v2=int(input("v2:"))
+
+# V=v1+v2
+# T = (t1 * v1 + t2 * v2) / (v1 + v2)
+# print("Hajmi:",V, "Tempratura:",T)
