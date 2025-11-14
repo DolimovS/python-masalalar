b9f5893bacb4ef875320c749d86d6e7a473e1027
DolimovS <samandardolimov71@gmail.com>
2025-11-14 21:08:03 +0500

    if else

---
+++ 1.3 /main.py
@@ -0,0 +1,53 @@
+# # 1
+
+# x=int(input("x="))
+# y=int(input("y="))
+
+# # a 
+# if x>y:
+#     print("max=",x)
+# elif x<y:
+#     print("max:",y)
+# else:
+#     print("ikkisi ham teng ")
+    
+# #b
+# if x>y:
+#     print("min=",y)
+# elif x<y:
+#     print("min:",x)
+# else:
+#     print("ikkisi ham teng ")
+# #c
+# if x>y:
+#     max=x
+#     min=y
+# else:
+#     min=x
+#     max=y
+
+# p=3*max*2*min
+# print(p)
+
+# =====================================================================
+# # 4
+# a=int(input("a="))
+# b=int(input("b="))
+# c=int(input("c="))
+
+# if a >= b and b>=c:
+#  print(f"{2*a} \n{2*b}\n{2*c}")
+# =====================================================================
+# # 9. Berilgan uch xonali N sonning birinchi raqami toq sonmi?
+# n=int(input("n="))
+
+# birinchi_raqam=n//100
+# if birinchi_raqam%2==1:
+#     print("Toq")
+# else:
+#     print("Juft")
+# =====================================================================
+# Uchta a, b va c haqiqiy sonlar berilgan bo‘lsin. Shu sonlar uchun
+# ax2+bx-c=0 tenglamaning haqiqiy yechimlari mavjudmi? Agar mavjud bo‘lsa, bu
+# yechimlarni aniqlang.
+
